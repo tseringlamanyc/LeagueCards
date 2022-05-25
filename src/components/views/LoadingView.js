884e98421cc3ddd9231d1f6fd5f07328c9957614
Tsering Lama <tseringglamaa@gmail.com>
2022-05-25 13:42:00 -0400

    more changes

--- src/components/views/LoadingView.js
+++ src/components/views/LoadingView.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./EmptyList.scss";
 
 function LoadingView() {
-  return <div>Loading...</div>;
+  return <div className="emptyList">Loading...</div>;
 }
 
 export default LoadingView;
