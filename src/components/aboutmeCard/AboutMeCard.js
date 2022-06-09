32d982345d19c2cc1c97e2743d1f1b5474c5ed08
Tsering Lama <tseringglamaa@gmail.com>
2022-06-09 02:58:02 -0400

    started about me

---
+++ src/components/aboutmeCard/AboutMeCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+
+import "./AboutMeCard.scss";
+import AboutMeInfo from "../aboutmeInfo/AboutMeInfo";
+
+function AboutMeCard() {
+  return (
+    <div className="about-wrapper">
+      <div className="about-left">
+        <div className="about-left-content">
+          <div>
+            <div className="shadow">
+              <div className="about-img">
+                <img src="./my-avatar.png" alt="about image" />
+              </div>
+            </div>
+
+            <h2>Tsering Lama</h2>
+            <h3>Full Stack Web developer</h3>
+          </div>
+
+          <ul className="icons">
+            <li>
+              <GitHubIcon />
+            </li>
+            <li>
+              <TwitterIcon />
+            </li>
+            <li>
+              <LinkedInIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+      <AboutMeInfo />
+    </div>
+  );
+}
+
+export default AboutMeCard;
--- src/screens/aboutMe/AboutMe.js
+++ src/screens/aboutMe/AboutMe.js
@@ -1,7 +1,14 @@
 import React from "react";
+import AboutMeCard from "../../components/aboutmeCard/AboutMeCard";
+import AboutMeInfo from "../../components/aboutmeInfo/AboutMeInfo";
+import "./AboutMe.scss";
 
 function AboutMe() {
-  return <div>AboutMe</div>;
+  return (
+    <div className="aboutme">
+      <AboutMeCard />
+    </div>
+  );
 }
 
 export default AboutMe;
