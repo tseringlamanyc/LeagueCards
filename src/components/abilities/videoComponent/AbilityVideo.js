350a0e37e81a51eafc4e57ef783aa509ac708ada
Tsering Lama <tseringglamaa@gmail.com>
2022-06-11 03:51:51 -0400

    changes once more

--- src/components/abilities/videoComponent/AbilityVideo.js
+++ src/components/abilities/videoComponent/AbilityVideo.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./AbilityVideo.scss";
 
 function AbilityVideo({ abilityUrl }) {
-  return <video className="abilityVideo" src={abilityUrl} autoPlay type="video/mp4"></video>;
+  return (
+    <video className="abilityVideo" src={abilityUrl} autoPlay playsInline type="video/mp4"></video>
+  );
 }
 
 export default AbilityVideo;
